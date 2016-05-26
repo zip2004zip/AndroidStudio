9a4f5d81d7923383b233c9b03fa0aaf02dc9f14f
Sleekeer <sleekeer.gog@gmail.com>
2016-05-26 21:04:15 +0300

    TODO

--- app/src/main/java/com/example/st/myapplication/MainActivity.java
+++ app/src/main/java/com/example/st/myapplication/MainActivity.java
@@ -1,16 +1,19 @@
 package com.example.st.myapplication;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -25,19 +28,69 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(this);
+
+        Button button = (Button) findViewById(R.id.newActivity1Button);
+        button.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        final TextView textView = (TextView) findViewById(R.id.textId);
-        textView.setText("Text v MainActivity"); // изменяем текст после нажатия
-        textView.setTextColor(Color.BLUE);
-        textView.setTextColor(getResources().getColor(R.color.colorTextPosleNazhatia));  //изменяем цвет текста после нажатия
-        textView.setBackgroundColor(getResources().getColor(R.color.colorFonPosleNazhatia)); // изменяем фон текста после нажатия
-
-        EditText editText = (EditText) findViewById(R.id.editText);
-        textView.setText(editText.getText());
+        switch (v.getId()) {
+            case R.id.fab:
+                //  textView.setTextColor(Color.BLUE);
+                final TextView textView = (TextView) findViewById(R.id.textId);
+                textView.setTextColor(getResources().getColor(R.color.colorTextPosleNazhatia));  //изменяем цвет текста после нажатия
+                textView.setBackgroundColor(getResources().getColor(R.color.colorFonPosleNazhatia)); // изменяем фон текста после нажатия
+                EditText editText = (EditText) findViewById(R.id.editText);
+                textView.setText(editText.getText());
+                textView.setText("Text v MainActivity"); // изменяем текст после нажатия
+                Toast.makeText(this, "текст изменен успешно", Toast.LENGTH_LONG).show(); // всплываемое оно
+                Log.d("TAG", "msg d");
+                Log.e("TAG", "msg e");
+                break;
+            case R.id.newActivity1Button:
+                Intent intent = new Intent(this, Main2Activity.class);
+                startActivity(intent);
+                break;
+        }
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "Старт", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this, "Рестарт", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "Возобновить", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "Пауза", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "Стоп", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "Уничтожить", Toast.LENGTH_SHORT).show();
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
