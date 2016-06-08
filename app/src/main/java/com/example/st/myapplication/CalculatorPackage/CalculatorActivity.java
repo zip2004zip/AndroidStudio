8d9e81d77e3034f595c8b67694515539e5e8dd92
zip2004zip <zip2004zip@mail.ru>
2016-06-09 01:51:03 +0300

    Calculate

--- app/src/main/java/com/example/st/myapplication/CalculatorPackage/CalculatorActivity.java
+++ app/src/main/java/com/example/st/myapplication/CalculatorPackage/CalculatorActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.st.myapplication.CalculatorPackage.Operations;
 import com.example.st.myapplication.R;
@@ -12,9 +13,14 @@ import com.example.st.myapplication.R;
 public class CalculatorActivity extends AppCompatActivity implements View.OnClickListener {
 
     private EditText editText;
+
+    Button buttonSum;
+    Button buttonMinus;
+    Button buttonMultiply;
+    Button buttonDivide;
     private Operations operations = null;
-    private int a = 0;
-    private int b = 0;
+    private double a = 0;
+    private double b = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +28,16 @@ public class CalculatorActivity extends AppCompatActivity implements View.OnClic
         setContentView(R.layout.activity_calculator);
 
         editText = (EditText) findViewById(R.id.editText2);
-        // editText.getText();
 
-        // Button buttonSum = (Button) findViewById(R.id.buttonSum);
-        //  buttonSum.setOnClickListener(this);
+        buttonSum = (Button) findViewById(R.id.buttonSum);
+        buttonMinus = (Button) findViewById(R.id.buttonMinus);
+        buttonMultiply = (Button) findViewById(R.id.buttonMultiply);
+        buttonDivide = (Button) findViewById(R.id.buttonDivide);
+
+        buttonSum.setTag(Operations.SUMMA);
+        buttonMinus.setTag(Operations.MINUS);
+        buttonMultiply.setTag(Operations.MULTIPLY);
+        buttonDivide.setTag(Operations.DIVIDE);
 
     }
 
@@ -33,33 +45,32 @@ public class CalculatorActivity extends AppCompatActivity implements View.OnClic
     @Override
     public void onClick(View v) {
 
-
         switch (v.getId()) {
 
             case R.id.buttonSum: {
                 operations = Operations.SUMMA;
-                a = Integer.valueOf(editText.getText().toString());
+                a = Double.valueOf(editText.getText().toString());
                 editText.setText("");
                 break;
             }
 
             case R.id.buttonMinus: {
                 operations = Operations.MINUS;
-                a = Integer.valueOf(editText.getText().toString());
+                a = Double.valueOf(editText.getText().toString());
                 editText.setText("");
                 break;
             }
 
             case R.id.buttonMultiply: {
                 operations = Operations.MULTIPLY;
-                a = Integer.valueOf(editText.getText().toString());
+                a = Double.valueOf(editText.getText().toString());
                 editText.setText("");
                 break;
             }
 
             case R.id.buttonDivide: {
                 operations = Operations.DIVIDE;
-                a = Integer.valueOf(editText.getText().toString());
+                a = Double.valueOf(editText.getText().toString());
                 editText.setText("");
                 break;
             }
@@ -70,28 +81,70 @@ public class CalculatorActivity extends AppCompatActivity implements View.OnClic
             }
 
             case R.id.buttonDelete: {
-
+                editText.setText(editText.getText().delete(editText.getText().length() - 1, editText.getText().length()));
+                if (editText.getText().toString().trim().length() == 0) {
+                    editText.setText("0");
+                }
                 break;
             }
 
             case R.id.buttonComma: {
-
+                //  editText.setText("0" + v.getContentDescription().toString());
+                if (!editText.getText().toString().contains(".")) {
+                    editText.setText(editText.getText() + ".");
+                }
                 break;
             }
 
             case R.id.buttonResult: {
-                b = Integer.valueOf(editText.getText().toString());
-                editText.setText("");
+                b = Double.valueOf(editText.getText().toString());
+                // editText.setText("");
                 switch (operations) {
                     case SUMMA:
-                        editText.setText(String.valueOf(a + b));
+                        if (a + b % 1 == 0) {
+                            editText.setText(String.valueOf((int) a + b));
+                        } else {
+                            editText.setText(String.valueOf(a + b));
+                        }
+                        break;
+
+                    case MINUS:
+                        if (a - b % 1 == 0) {
+                            editText.setText(String.valueOf((int) a - b));
+                        } else {
+                            editText.setText(String.valueOf(a - b));
+                        }
+                        break;
+
+                    case DIVIDE:
+                        if (b == 0) {
+                            Toast.makeText(this, "На ноль делить нельзя!\nВведите число еще раз", Toast.LENGTH_LONG).show();
+                            return;
+                        }
+                        if (a / b % 1 == 0) {
+                            editText.setText(String.valueOf((int) a / b));
+                        } else {
+                            editText.setText(String.valueOf(a / b));
+                        }
+                        break;
+
+                    case MULTIPLY:
+                        if (a * b % 1 == 0) {
+                            editText.setText(String.valueOf((int) a * b));
+                        } else {
+                            editText.setText(String.valueOf(a * b));
+                        }
                         break;
                 }
                 break;
             }
 
             default: {
-                editText.setText(editText.getText() + v.getContentDescription().toString());
+                if (editText.getText().toString().equals("0")) { //что бы спереди когда начинаем вводить цифры не стоял сразу 0, то он удаляется 092
+                    editText.setText(v.getContentDescription().toString());
+                } else {
+                    editText.setText(editText.getText() + v.getContentDescription().toString());
+                }
             }
         }
     }
