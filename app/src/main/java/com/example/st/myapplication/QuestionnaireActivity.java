24183d5d0e2bda3599c9e5b88674117552ac91b8
zip2004zip <zip2004zip@mail.ru>
2016-06-13 01:25:11 +0300

    Questionnaire

--- app/src/main/java/com/example/st/myapplication/QuestionnaireActivity.java
+++ app/src/main/java/com/example/st/myapplication/QuestionnaireActivity.java
@@ -2,12 +2,29 @@ package com.example.st.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class QuestionnaireActivity extends AppCompatActivity {
+public class QuestionnaireActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText editText;
+    Button buttonSend;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_questionnaire);
+
+        editText = (EditText) findViewById(R.id.editText_Suggestion);
+
+        buttonSend = (Button) findViewById(R.id.button_Send);
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
