640e45c0c95f0be78c8ce1eee0e279b88a24e265
zip2004zip <zip2004zip@mail.ru>
2016-06-14 01:36:42 +0300

    Questionnaire

--- app/src/main/java/com/example/st/myapplication/QuestionnaireActivity.java
+++ app/src/main/java/com/example/st/myapplication/QuestionnaireActivity.java
@@ -5,20 +5,32 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class QuestionnaireActivity extends AppCompatActivity implements View.OnClickListener {
 
     private EditText editText;
     Button buttonSend;
+    private static SeekBar seekBar;
+    private static TextView textView_seekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_questionnaire);
 
+        // SeekBar
+        seekBar = (SeekBar) findViewById(R.id.seekBar_nights);
+        textView_seekBar = (TextView) findViewById(R.id.textView_nights_seekBar);
+        seekBar();
+
+        // пожелания
         editText = (EditText) findViewById(R.id.editText_Suggestion);
 
+        // кнопка отправки
         buttonSend = (Button) findViewById(R.id.button_Send);
+
 
     }
 
@@ -27,4 +39,23 @@ public class QuestionnaireActivity extends AppCompatActivity implements View.OnC
     public void onClick(View v) {
 
     }
+
+    public void seekBar() {
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                // textView_seekBar.setText(String.valueOf(progress) + "дней");
+                textView_seekBar.setText(progress + " дней");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+    }
+
 }
