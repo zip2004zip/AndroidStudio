5b4f84de61a675d5e8a5c33be058941953e4ff36
zip2004zip <zip2004zip@mail.ru>
2016-06-16 01:33:09 +0300

    Questionnaire

--- app/src/main/java/com/example/st/myapplication/Quesionnaire/Questionnaire.java
+++ app/src/main/java/com/example/st/myapplication/Quesionnaire/Questionnaire.java
@@ -1,8 +1,112 @@
 package com.example.st.myapplication.Quesionnaire;
 
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 /**
  * Created by Sergei on 14.06.2016.
  */
 public class Questionnaire {
+    private String firstName;
+    private String lastName;
+    private Date dateAndTimeOfArrive;
+    private int nights;
+    private String sex;
+    private boolean breakFast;
+    private boolean dinner;
+    private boolean supper;
+    private boolean fullBoard;
+    private String city;
+    private String suggestions;
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getDateAndTimeOfArrive() {
+        return dateAndTimeOfArrive;
+    }
+
+    public void setDateAndTimeOfArrive(GregorianCalendar dateAndTimeOfArrive) {
+      //  this.dateAndTimeOfArrive = dateAndTimeOfArrive;
+        Date date;
+        date = new Date(dateAndTimeOfArrive.getTimeInMillis());
+        this.dateAndTimeOfArrive = date;
+    }
+
+    public int getNights() {
+        return nights;
+    }
+
+    public void setNights(int nights) {
+        this.nights = nights;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public boolean isBreakFast() {
+        return breakFast;
+    }
+
+    public void setBreakFast(boolean breakFast) {
+        this.breakFast = breakFast;
+    }
+
+    public boolean isDinner() {
+        return dinner;
+    }
+
+    public void setDinner(boolean dinner) {
+        this.dinner = dinner;
+    }
+
+    public boolean isSupper() {
+        return supper;
+    }
+
+    public void setSupper(boolean supper) {
+        this.supper = supper;
+    }
+
+    public boolean isFullBoard() {
+        return fullBoard;
+    }
+
+    public void setFullBoard(boolean fullBoard) {
+        this.fullBoard = fullBoard;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getSuggestions() {
+        return suggestions;
+    }
+
+    public void setSuggestions(String suggestions) {
+        this.suggestions = suggestions;
+    }
 }
--- app/src/main/java/com/example/st/myapplication/Quesionnaire/QuestionnaireActivity.java
+++ app/src/main/java/com/example/st/myapplication/Quesionnaire/QuestionnaireActivity.java
@@ -1,26 +1,38 @@
 package com.example.st.myapplication.Quesionnaire;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import com.example.st.myapplication.R;
 
-public class QuestionnaireActivity extends AppCompatActivity implements View.OnClickListener, RadioGroup.OnCheckedChangeListener {
+import java.text.SimpleDateFormat;
+import java.util.GregorianCalendar;
+
+public class QuestionnaireActivity extends AppCompatActivity implements View.OnClickListener, RadioGroup.OnCheckedChangeListener, DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
 
     private EditText eTextFirstName;
     private EditText eTextLastName;
 
+    private GregorianCalendar gregorianCalendar;
     private TextView tViewDate;
+    private DatePickerDialog datePickerDialog;
     private TextView tViewTime;
+    private TimePickerDialog timePickerDialog;
 
     private SeekBar seekBar;
     private TextView tViewSeekBar;
+
+
 
     private TextView tViewSex;
     private RadioGroup radioGroup;
@@ -28,6 +40,8 @@ public class QuestionnaireActivity extends AppCompatActivity implements View.OnC
     private EditText eTextSuggestion;
 
     private Button buttonSend;
+
+    private Questionnaire questionnaire;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,8 +52,14 @@ public class QuestionnaireActivity extends AppCompatActivity implements View.OnC
         eTextFirstName = (EditText) findViewById(R.id.editTextFirstName);
         eTextLastName = (EditText) findViewById(R.id.editTextLastName);
 
+        // Date and Time of arrive
+        gregorianCalendar = new GregorianCalendar();
         tViewDate = (TextView) findViewById(R.id.textViewDate);
+        datePickerDialog = new DatePickerDialog(this, this, gregorianCalendar.get(GregorianCalendar.YEAR), gregorianCalendar.get(GregorianCalendar.MONTH), gregorianCalendar.get(GregorianCalendar.DAY_OF_MONTH));
+
         tViewTime = (TextView) findViewById(R.id.textViewTime);
+        timePickerDialog = new TimePickerDialog(this, this, gregorianCalendar.get(GregorianCalendar.HOUR_OF_DAY), gregorianCalendar.get(GregorianCalendar.MINUTE), true);
+
 
         // SeekBar
         seekBar = (SeekBar) findViewById(R.id.seekBarNights);
@@ -58,7 +78,33 @@ public class QuestionnaireActivity extends AppCompatActivity implements View.OnC
 
     @Override
     public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.textViewDate:
+                datePickerDialog.show();
+                break;
+            case R.id.textViewTime:
+                timePickerDialog.show();
+                break;
+            case R.id.buttonSend:
+                Questionnaire questionnaire = createQuestionnaire();
 
+
+
+        }
+    }
+
+    private Questionnaire createQuestionnaire() {
+        questionnaire = new Questionnaire();
+        questionnaire.setFirstName(eTextFirstName.getText().toString());
+        questionnaire.setLastName(eTextLastName.getText().toString());
+
+        questionnaire.setDateAndTimeOfArrive(gregorianCalendar);
+
+        questionnaire.setNights(Integer.valueOf(tViewSeekBar.getText().toString()));
+
+
+
+        return questionnaire;
     }
 
     public void seekBar() {
@@ -82,6 +128,21 @@ public class QuestionnaireActivity extends AppCompatActivity implements View.OnC
 
     @Override
     public void onCheckedChanged(RadioGroup group, int checkedId) {
-      //  radioGroup.setOnCheckedChangeListener(new RadioGroup());
+        //  radioGroup.setOnCheckedChangeListener(new RadioGroup());
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+        gregorianCalendar.set(year, monthOfYear, dayOfMonth);
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd MMM yyyy");
+        tViewDate.setText(simpleDateFormat.format(gregorianCalendar.getTime()));
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        gregorianCalendar.set(GregorianCalendar.HOUR_OF_DAY, hourOfDay);
+        gregorianCalendar.set(GregorianCalendar.MINUTE, minute);
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm");
+        tViewTime.setText(simpleDateFormat.format(gregorianCalendar.getTime()));
     }
 }
